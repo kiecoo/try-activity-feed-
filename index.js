a329e27051bc9564289e8f3d7489a38fbb3f5038
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-03-07 10:44:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,3 @@ module.exports = getGithubData
 
 
 
-var exampleProfile = {
-  name: 'nina',
-  username: 'ninabreznik',
-  photo:'https://nomadlist.com/assets/img/cities/phuket-thailand-500px.jpg'
-}
