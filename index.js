c2a8a481eaf04d5031741b1419f9bc3d92b9f525
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-03-15 16:37:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ var exampleProfile = {
 }
 
 
-
+	        //difference from the repo of makeGitterChatbox https://github.com/kiecoo/makeGitterChatbox/blob/master/index.html
+		var x = makeGitterChatbox(exampleProfile)
+		document.body.appendChild(x)
 
